2686c56b201bf1cff1a683f1ad84d73647a37798
duarteeder <ederson.duarte@acct.global>
2023-10-19 15:53:25 -0300

    fixing bean bug

--- src/main/java/app/config/KafkaConfig.java
+++ src/main/java/app/config/KafkaConfig.java
@@ -1,5 +1,7 @@
 package app.config;
 
+import app.model.JobPostedEvent;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -11,6 +13,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,24 +23,25 @@ import java.util.Map;
 public class KafkaConfig {
 
     @Bean
-    public ProducerFactory<String, String> producerFactory() {
+    public ProducerFactory<String, JobPostedEvent> producerFactory() {
         Map<String, Object> producerProps = new HashMap<>();
-        producerProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "your-broker-address"); // Kafka broker address
-        producerProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
-        producerProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        producerProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        producerProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        producerProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class); // Assuming you want to serialize JobPostedEvent as JSON
 
         return new DefaultKafkaProducerFactory<>(producerProps);
+
     }
 
     @Bean
-    public KafkaTemplate<String, String> kafkaTemplate() {
+    public KafkaTemplate<String, JobPostedEvent> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
     }
 
     @Bean
     public ConsumerFactory<String, String> consumerFactory() {
         Map<String, Object> consumerProps = new HashMap<>();
-        consumerProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "your-broker-address");
+        consumerProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         consumerProps.put(ConsumerConfig.GROUP_ID_CONFIG, "your-consumer-group-id");
         consumerProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
         consumerProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
--- src/main/java/app/service/KafkaProducerService.java
+++ src/main/java/app/service/KafkaProducerService.java
@@ -5,12 +5,11 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
 @Service
 public class KafkaProducerService {
 
     private final KafkaTemplate<String, JobPostedEvent> kafkaTemplate;
-    private static final String TOPIC = "job-posted-topic"; // Replace with your topic name
+    private static final String TOPIC = "job-posted-topic";
 
     @Autowired
     public KafkaProducerService(KafkaTemplate<String, JobPostedEvent> kafkaTemplate) {
